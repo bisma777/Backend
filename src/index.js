2cbea6ee714ab1a9fdecad8ca367e9c344bba459
bisma <nazirbism7@gmail.com>
2024-04-03 21:56:36 +0530

    utils discussion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server started on port ${process.env.PORT}`)
+    );
+  })
+  .catch((error) => {
+    console.log("connection failed", error);
+  });
 /*
 
 import express from "express";
