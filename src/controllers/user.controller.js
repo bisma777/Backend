8c9faa59110912e7b92d2d3021f805dc944c3e0a
bisma <nazirbism7@gmail.com>
2024-04-12 14:59:53 +0530

    postman updation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,20 +15,29 @@ const registerUser = asyncHandler(async (req, res) => {
   //check for user creation
   //return response
   const { fullName, email, username, password } = req.body;
-  console.log(email, "emaillll");
+
   if (
     [fullName, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All field are required");
   }
-  const existedUSer = User.findOne({
+  const existedUSer = await User.findOne({
     $or: [{ username }, { email }],
   });
   if (existedUSer) {
     throw new ApiError(409, "User already exists");
   }
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar files is required");
   }
